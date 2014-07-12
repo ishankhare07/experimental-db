87251da21ab654d779ce654a016ee85d67c63fb7
Ishan Khare <ishankhare07@gmail.com>
2014-07-12 19:57:39 +0530

    completed

--- main.py
+++ main.py
@@ -2,24 +2,41 @@ import tornado.websocket
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+import tornado.httpserver
 import anydbm
 import json
+import os
 
-users = {}	#{username : password}
+db = anydbm.open('test_db','n')
 
 class WsHandler(tornado.WebSocket.WebSocketHandler):
-	self.db = anydbm.open('main_db','n')	
 
 	def open(self):
-		if self not in users:
-			users.append(self)
-			self.write('welcome new user')
-		else:
-			self.write('welcome back')
+		self.write_message('Welcome\n')
 
 	def on_message(self,json_message):
 		message = json.loads(json_message)
 
-
 		if message[0].strip() is 'add':
-			
+			key = message[1].strip()
+			value = message[2].strip()
+			db[key] = value
+
+		elif message[0].strip() is 'show':
+			dict_str = json.dumps(db)
+			self.write_message(dict_str)
+
+		else:
+			self.write_message('wrong format!')
+
+	def on_close(self):
+		self.send_message('goodbye...')
+
+app = tornado.web.Application([
+	(r'/',WsHandler),
+])
+
+server = tornado.httpserver.HTTPServer(app)
+port = int(os.environ.get("PORT",5000))
+server.listen(port)
+tornado.ioloop.IOLoop.instance().start()
